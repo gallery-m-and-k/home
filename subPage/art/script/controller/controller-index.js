595263b434db9df9f8f0c4d56046d21fbaf8795f
DESKTOP-QE6N5BC\茶の助 <tyanosuke_dcgsk@yahoo.co.jp>
2023-08-10 16:05:00 +0900

    いろいろ調整２。

--- subPage/art/script/controller/controller-index.js
+++ subPage/art/script/controller/controller-index.js
@@ -7,32 +7,27 @@ window.onpageshow = function (event) {
 
 $(document).ready(function () {
   // ================================================================
-
   /**
    * 初期化
    */
-
-  // style削除
-  const buttons = document.querySelectorAll(".categoryButton");
-  buttons.forEach((element) => {
-    element.style = {};
-  });
-
-  // ギャラリー非表示
-  const containers = document.querySelectorAll(".categoryButton");
-  containers.forEach((element) => {
-    element.addEventListener("click", () => {
-      // 押されたほうを拡大表示
-      element.classList.add("select");
-
-      // もう一回押す
-      element.addEventListener("click", () => {
-        // リンク遷移
-        const category = element.getAttribute("data-link");
-        open(category, "_self");
-      });
-    });
-  });
-
+  // // style削除
+  // const buttons = document.querySelectorAll(".categoryButton");
+  // buttons.forEach((element) => {
+  //   element.style = {};
+  // });
+  // // ギャラリー非表示
+  // const containers = document.querySelectorAll(".categoryButton");
+  // containers.forEach((element) => {
+  //   element.addEventListener("click", () => {
+  //     // 押されたほうを拡大表示
+  //     element.classList.add("select");
+  //     // もう一回押す
+  //     element.addEventListener("click", () => {
+  //       // リンク遷移
+  //       const category = element.getAttribute("data-link");
+  //       open(category, "_self");
+  //     });
+  //   });
+  // });
   // ================================================================
 });
